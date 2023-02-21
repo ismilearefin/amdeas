77a80caad23a01104490c892beda9a50b7de0fd9
ismilearefin <arefinismile@gmail.com>
2023-02-21 17:37:55 +0600

    Mouse scroll animation

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import vector from '../../assets/Vector3.png'
 import vector2 from '../../assets/Vector2.svg'
-import mouse from '../../assets/mouse.png'
+// import mouse from '../../assets/mouse.png'
+import { BsDot } from 'react-icons/bs';
 
 
 const HeroSection = () => {
@@ -10,7 +11,11 @@ const HeroSection = () => {
             <img src={vector2} alt='b' className='blur-[6.25rem] absolute bottom-20 w-[37.5rem]'></img>
             <img src={vector} alt='a' className='blur-[6.25rem] absolute top-[15.625rem] w-[31.25rem]'></img>
             <h1 className='text-center relative z-10 text-[4rem] text-white font-display font-bold'>Amdeas group of companies<br/> consists of agency services and<br/> software products</h1>
-            <img src={mouse} alt='mouse' className='absolute bottom-[6.25rem]'/>   
+            <div className='absolute bottom-[6.25rem]'>
+                <div className='bg-[#ffffff1a] rounded-[50px] w-6 h-[40px] flex justify-center border-[#ffffff33] border-[1.5px]'>
+                    <BsDot className='text-[#CFC3FF] animate-bounce'></BsDot>
+                </div>
+            </div>
         </div>
     );
 };
